5ff036cc5e60df8a05320bc429547c1173099bc2
Sundeep-D <sundeep0412@gmail.com>
2024-04-18 23:36:45 -0600

    Basic App

--- Project/Project_Code/server/server.js
+++ Project/Project_Code/server/server.js
@@ -1,14 +1,16 @@
 const net = require('net');
 const { v4: uuidv4 } = require('uuid');
-const websocketPort = 8001;
-const tcpPort = 8000;
+const WebSocket = require('ws');
 const clear = require('cli-clear');
 const insertData = require('./databaseUtils');
-const WebSocket = require('ws');
-const constants = require('./constants'); 
+const constants = require('./constants');
+
+const websocketPort = 8001;
+const tcpPort = 8000;
 
 const wss = new WebSocket.Server({ port: websocketPort });
 console.log(`Websocket Server running on ${constants.host}:${websocketPort}`);
+
 wss.on('connection', function connection(ws) {
   console.log('Websocket Client connected');
 
@@ -18,16 +20,13 @@ wss.on('connection', function connection(ws) {
     ws.send(message);
   });
 
-  ws.on('close', function() {
+  ws.on('close', function () {
     console.log('Websocket Client disconnected');
   });
 });
 
-
 const server = net.createServer(socket => {
   console.log('Client connected');
-
-  
 
   // Generate a unique key
   const uniqueKey = generateUniqueKey();
@@ -35,19 +34,18 @@ const server = net.createServer(socket => {
   socket.on('data', data => {
     const jsonData = parseJson(data.toString());
     if (jsonData) {
-        clear();
-        console.log('UNOR4:', jsonData);
-        wss.clients.forEach(client => {
-          if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify(jsonData));
-          }else{
-            // console.log('Error sending data to websocket clients:');
-          }
+      clear();
+      console.log('UNOR4:', jsonData);
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(jsonData));
+        } else {
+          // console.log('Error sending data to websocket clients:');
+        }
       });
-        insertData(jsonData).catch(console.error);
-       
+      insertData(jsonData).catch(console.error);
     } else {
-        // console.log('Invalid JSON data or empty:', data.toString());
+      // console.log('Invalid JSON data or empty:', data.toString());
     }
 
     if (jsonData) {
@@ -58,7 +56,6 @@ const server = net.createServer(socket => {
         }
       });
     }
-
   });
 
   socket.on('end', () => {
@@ -76,36 +73,31 @@ const server = net.createServer(socket => {
     const key = generateUniqueKey();
     socket.write(key);
     console.log(`Sent ${key} to Arduino.`);
-    }, 10000);
+  }, 10000);
 
-  console.log(`Handshake successfull! Sent ${uniqueKey} to Arduino.`);
+  console.log(`Handshake successful! Sent ${uniqueKey} to Arduino.`);
 });
 
-
-
-
-
 function parseJson(dataString) {
-    try {
-        const jsonData = JSON.parse(dataString);
-        if (Object.keys(jsonData).length === 0 && jsonData.constructor === Object) {
-            // Check if it's empty JSON
-            return null;
-        }
-        return jsonData;
-    } catch (error) {
-        // If parsing fails, it's not valid JSON
-        return null;
+  try {
+    const jsonData = JSON.parse(dataString);
+    if (Object.keys(jsonData).length === 0 && jsonData.constructor === Object) {
+      // Check if it's empty JSON
+      return null;
     }
+    return jsonData;
+  } catch (error) {
+    // If parsing fails, it's not valid JSON
+    return null;
+  }
 }
 
 server.listen(tcpPort, () => {
   const address = server.address();
   console.log(`TCP Server running on ${constants.host}:${address.port}`);
-  
 });
 
 // Function to generate a unique key
 function generateUniqueKey() {
-    return uuidv4(); //added
+  return uuidv4();
 }
